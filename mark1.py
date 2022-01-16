65e174c7de8bed330e169edcca4a744f5a5a9605
ileuxali <92287421+ileuxali@users.noreply.github.com>
2022-01-15 20:38:48 -0500

    Add files via upload

---
+++ mark1.py
@@ -0,0 +1,17 @@
+"""
+Data preprocessing
+"""
+import numpy as np
+import pandas as pd
+
+raw_df = pd.read_csv('news_summary_more.csv', encoding='iso-8859-1')
+listed_df = pd.DataFrame
+
+
+def to_list(text: pd.DataFrame) -> list:
+    """Make text from a single string into a list of strings."""
+    return [word for word in text]
+
+
+listed_df['summary'] = raw_df['summary'].apply(lambda entry: to_list(entry))
+listed_df['text'] = raw_df['text'].apply(lambda entry: to_list(entry))
